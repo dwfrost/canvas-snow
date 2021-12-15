314c7527790602e5fa0e8f7fa6e95b183790cb83
dengwei <dengw@rp-pet.cn>
2021-12-15 18:25:13 +0800

    feat: 改造雪花

--- snowflake.js
+++ snowflake.js
@@ -4,11 +4,11 @@
 */
 
 ;(function (window, document) {
-    var canvas = document.createElement('canvas')
-    var ctx = canvas.getContext('2d')
-    var style = canvas.style
-    var width, height, count
-    var snowflakes, snowflake
+    let canvas = document.createElement('canvas')
+    let ctx = canvas.getContext('2d')
+    let style = canvas.style
+    let width, height, count
+    let snowflakes, snowflake
 
     style.position = 'fixed'
     style.top = '0'
@@ -22,29 +22,45 @@
      * @constructor
      */
 
-    function Snowflake() {
+    function Snowflake(options = {}) {
+        let defaultOptions = { 
+            scale = 0.2
+            , speed = 1,
+              opacity 
+            }
+        options = {...defaultOptions,...options}
         this.x = randomize(width)
         this.y = randomize(height * -1)
-        this.r = 0.5 + randomize(3)
-        this.o = 0.3 + randomize(0.7)
-        this.vy = 1 + randomize(3)
-        this.vx = 0.5 - randomize()
+        // this.scale = scale
+        this.scale = 0.2
+        this.opacity = opacity || 0.3 + randomize(0.7) // 透明度
+        this.vy = 1 * speed + randomize(3) // y轴速度
+        this.vx = 0.5 * speed - randomize() // x轴速度
     }
 
-    function init() {
+    function init(callback) {
+        console.log('count', count)
         snowflakes = new Array(count)
 
-        for (var i = 0; i < count; i++) {
-            snowflakes[i] = new Snowflake()
+        let img = new Image()
+        this.__img = img
+        img.src = './snow.png'
+        img.onload = () => {
+            for (let i = 0; i < count; i++) {
+                snowflakes[i] = new Snowflake()
+            }
+            callback(img)
         }
     }
 
-    function update() {
-        window.requestAnimationFrame(update)
+    function update(img) {
+        window.requestAnimationFrame(() => {
+            update(img)
+        })
 
         ctx.clearRect(0, 0, width, height)
 
-        for (var i = 0; i < count; i++) {
+        for (let i = 0; i < count; i++) {
             snowflake = snowflakes[i]
             snowflake.y += snowflake.vy
             snowflake.x += snowflake.vx
@@ -52,9 +68,9 @@
             if (snowflake.y > height || snowflake.x > width || snowflake.x < 0) {
                 snowflakes[i] = new Snowflake()
             } else if (snowflake.y > 0) {
-                ctx.globalAlpha = snowflake.o
+                ctx.globalAlpha = snowflake.opacity
                 ctx.beginPath()
-                ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false)
+                ctx.drawImage(img, snowflake.x, snowflake.y, img.width * snowflake.scale, img.height * snowflake.scale)
                 ctx.closePath()
                 ctx.fill()
             }
@@ -62,7 +78,7 @@
     }
 
     function resize() {
-        var element = document.documentElement,
+        let element = document.documentElement,
             body = document.body
 
         width = window.innerWidth || element.clientWidth || body.clientWidth
@@ -72,8 +88,6 @@
         canvas.width = width
         canvas.height = height
         ctx.fillStyle = '#FFF'
-
-        init()
     }
 
     /**
@@ -87,7 +101,8 @@
 
     function load() {
         resize()
-        update()
+        init(update)
+        // update()
 
         document.body.appendChild(canvas)
     }
